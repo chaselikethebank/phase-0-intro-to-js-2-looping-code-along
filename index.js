214a4b5185d4a6ce92a5aa5a5c7a52dc4e98ef76
Chase <chasedemaster.tech@gmail.com>
2022-05-26 14:55:37 -0500

    commit tree 548b1d4adfde0d81637c91239351ad67f5b66f19

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solutions in this file
+
+// const cards = ["anniversay", "bday", "new years"]
+// const events = ["holly", "mom", "teddy"]
+
+// function writeCards(cards, events) {
+//     for (let i = 0; i < cards.length; i++) {
+//       console.log(`Well... ${cards[i]} cheers to your ${[i]}!`);
+//       // debugger;
+//     }
+  
+//     return cards;
+// }
+
+// writeCards(cards, events)
+
+
+const messages = []
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++)
+    messages.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    return messages
+}
+
+function countDown(num) {
+    let cd = (num);
+    while (cd >= 0) {
+        console.log(cd--);
+    }
+}
+
+
+countDown(10)
